1932d7394d0332645c56c8b75934f03db80d8011
Felipe Rodrigues <felipe_rodriguesx@hotmail.com>
2019-05-20 22:00:19 -0300

    implementando os metdos listAll e sumItems

--- public/app/app.js
+++ public/app/app.js
@@ -1,10 +1,12 @@
-import { handleStatus } from './utils/promise-helpers.js'
+import { log } from './utils/promise-helpers.js';
+import './utils/array-helpers.js';
+import { notasService as service} from './nota/service.js'
 
 document
     .querySelector('#myButton')
-    .onclick = () => 
-        fetch('http://localhost:3000/notas')
-            .then(handleStatus) //versao simplificade de .then(res => handleStatus(res))
+    .onclick = () =>
+            service
+            .sumItems('2143')
             .then(notas => console.log(notas))
-            .catch(console.log); // versao simplificada de .catch(err => console.log(err))
+            .catch(log); // versao simplificada de .catch(err => console.log(err))
 
