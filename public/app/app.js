7a572cf1dcd534669adac08cf0e06b01e4d5c8dc
Felipe Rodrigues <felipe_rodriguesx@hotmail.com>
2019-05-31 14:51:22 -0300

    adicionando o modelo pipe

--- public/app/app.js
+++ public/app/app.js
@@ -1,7 +1,22 @@
 import { log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
-import { notasService as service } from './nota/service.js'
-import { takeUtil, debounceTime } from './utils/operators.js'
+import { notasService as service } from './nota/service.js';
+import { takeUtil, debounceTime, pipe, partialize } from './utils/operators.js';
+
+//Para executar esse código é necessário que o pipe seja feito de forma invertida,
+// devido ao callback das funçoes
+
+// const operations = pipe(
+//     partialize(takeUtil, 3),
+//     partialize(debounceTime, 500)
+// );
+
+// const action = operations(() =>
+//     service
+//         .sumItems('2143')
+//         .then(notas => console.log(notas))
+//         .catch(log) // versao simplificada de .catch(err => console.log(err))
+// );
 
 const action = debounceTime(500,
     takeUtil(3, () =>
